bfc6b10e6551df35c3ff30372348ca434454b7f3
jameshennigan <james@thehennigans.co.uk>
2020-08-29 19:10:34 +0100

    fixed

--- test.js
+++ test.js
@@ -1,14 +1,34 @@
 const usefulFuncs = require("./usefulFuncs");
-
+const fs = require("fs").promises;
 async function main() {
-  const parsed = await usefulFuncs.parseCSV(
-    "./2017 UKPGE electoral data 4.csv"
+  let rawByCandidate = usefulFuncs.parseCSV(
+    "./HoC-GE2019-results-by-constituency-csv.csv"
   );
-  parsed.forEach((candidate, idx) => {
-    if (candidate.length !== 8) {
-      throw new Error("incorrect length on line: " + (parseInt(idx) + 1));
+  let rawByConstituency = usefulFuncs.parseCSV(
+    "HoC-GE2019-results-by-candidate-csv.csv"
+  );
+  rawData = [await rawByCandidate, await rawByConstituency];
+  let parsedData = [];
+  for (let data of rawData) {
+    let rows = [];
+    for (let [idx, row] of data.entries()) {
+      if (idx !== 0) {
+        let parsingData = {};
+        for (let [cellIdx, cell] of row.entries()) {
+          parsingData[data[0][cellIdx]] = cell;
+        }
+        rows.push(parsingData);
+      }
     }
-  });
-  console.log(parsed);
+    parsedData.push(rows);
+  }
+
+  // parsed.forEach((candidate, idx) => {
+  //   if (candidate.length !== 8) {
+  //     throw new Error("incorrect length on line: " + (parseInt(idx) + 1));
+  //   }
+  // });
+  console.log(rawByCandidate);
+  console.log(rawByConstituency);
 }
 main();
--- usefulFuncs.js
+++ usefulFuncs.js
@@ -1,4 +1,27 @@
-const { readdir, lstat, readFile } = require("fs").promises;
+const {
+  mkdir,
+  writeFile,
+  readdir,
+  lstat,
+  unlink,
+  readFile,
+  rmdir,
+} = require("fs").promises;
+
+async function save(tempString, quotedText, tempDir) {
+  await writeFile(`${tempDir}/${tempString}`, quotedText);
+}
+
+async function delDir(dirname) {
+  for (let file of await readdir(dirname)) {
+    if ((await lstat(dirname + "/" + file)).isDirectory()) {
+      await delDir(dirname);
+    } else {
+      await unlink(dirname + "/" + file);
+    }
+  }
+  await rmdir(dirname);
+}
 
 class usefulFuncs {
   //
@@ -12,7 +35,7 @@ class usefulFuncs {
         const recursiveFiles = await this.getDirsRecursively(
           sPath + "/" + item
         );
-        recursiveFiles.forEach(subfolderFile => {
+        recursiveFiles.forEach((subfolderFile) => {
           files.push(subfolderFile);
         });
       }
@@ -34,42 +57,48 @@ class usefulFuncs {
     }
     return text;
   }
-
   //
   async parseCSV(filePath) {
     let fileContents = await readFile(filePath, "utf-8");
-    let replaced = [];
     let quotedText;
     let firstQuoteLocation;
     let secondQuoteLocation;
     let tempString;
-    while (fileContents.indexOf('"') > -1) {
+    let tempDir = __dirname + "/" + this.generateRandomString(8);
+
+    await mkdir(tempDir);
+    while (fileContents.includes('"')) {
       firstQuoteLocation = fileContents.indexOf('"');
       secondQuoteLocation = fileContents.indexOf('"', firstQuoteLocation + 1);
       quotedText = fileContents.substring(
         firstQuoteLocation,
         secondQuoteLocation + 1
       );
-      tempString = this.generateRandomString(20);
+      tempString = this.generateRandomString(8);
       fileContents = fileContents.replace(quotedText, tempString);
-      replaced.push({
-        tempString,
-        quotedText
-      });
+
+      await save(tempString, quotedText, tempDir);
     }
     let rows = fileContents.replace(/\r/g, "").split("\n");
     for (let i = 0; i < rows.length; i++) {
-      rows[i] = rows[i].split(",");
-      rows[i].forEach((cell, idx) => {
-        let x;
-        for (x = 0; x < replaced.length; x++) {
-          if (cell === replaced[x].tempString) {
-            rows[i][idx] = replaced[x].quotedText;
-            break;
+      try {
+        rows[i] = rows[i].split(",");
+        let replaced = await readdir(tempDir);
+        for (let [idx, cell] of rows[i].entries()) {
+          let replacedIndex = replaced.indexOf(cell);
+          if (replacedIndex > -1) {
+            rows[i][idx] = await readFile(`${tempDir}/${cell}`, "utf8");
+            unlink(`${tempDir}/${cell}`);
+            replaced = replaced.splice(replaced.indexOf(cell), 1);
           }
         }
-      });
+      } catch (e) {
+        console.log(e);
+        delDir(`${tempDir}`);
+        throw e;
+      }
     }
+    delDir(`${tempDir}`);
     return rows;
   }
 }
